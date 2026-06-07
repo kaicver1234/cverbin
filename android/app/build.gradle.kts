b451f2487f43311c73d7390e04b3ba4f8f0eae14
kaicver1234 <tksrdmn@gmail.com>
2026-06-08 00:14:28 +0330

    TiksarVPN

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     defaultConfig {
         // Application ID for Tiksar VPN
         applicationId = "com.tiksarvpn.app"
-        minSdkVersion flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = 12
         versionName = "1.1.5"
